26b09cd8439a9214eeadada4ccfa3860b9dffc82
Thomas Schreiber <tom@rizu.mu>
2012-07-26 07:29:39 +0200

    whitespace

--- data/battery.sh
+++ data/battery.sh
@@ -3,13 +3,13 @@ PERCENT=`acpi -b | sed 's/\([^:]*\): \([^,]*\), \([0-9]*\)%.*/\3/'`
 STATUS=`acpi -b | sed 's/\([^:]*\): \([^,]*\), \([0-9]*\)%.*/\2/'`
 
 if [ $STATUS == "Charging" ]; then
-    if [ $PERCENT -ge 99 ]; then
-    	echo -n "ac: max"
-    else
-    	echo -n "ac: $PERCENT%"
-    fi
+  if [ $PERCENT -ge 99 ]; then
+    echo -n "ac: max"
+  else
+    echo -n "ac: $PERCENT%"
+  fi
 else
-  	echo -n "bat: $PERCENT%"
+  echo -n "bat: $PERCENT%"
 fi
 
 echo ""
