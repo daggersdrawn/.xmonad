663cdc8d8ffe5fe6b9a25d09ef51eb5321cef550
Thomas Schreiber <tom@rizu.mu>
2012-11-08 12:41:49 +0100

    bash fix for battery script

--- data/battery.sh
+++ data/battery.sh
@@ -2,17 +2,17 @@
 PERCENT=`acpi -b | sed 's/\([^:]*\): \([^,]*\), \([0-9]*\)%.*/\3/'`
 STATUS=`acpi -b | sed 's/\([^:]*\): \([^,]*\), \([0-9]*\)%.*/\2/'`
 
-
 if [ $STATUS == "Charging" ]; then
   if [ $PERCENT -ge 99 ]; then
-    echo -n "ac: max"
+    echo -n " ac: max"
   else
-    echo -n "ac: $PERCENT%"
+    echo -n " ac: $PERCENT%"
   fi
 else
-  if [ ! $STATUS contains "No support" ]; then
-    echo -n "bat: $PERCENT%"
+  if [[ ! $STATUS == *"No support"* ]]; then
+    echo -n " bat: $PERCENT%"
   fi
 fi
 
 echo ""
+if [[ "$string" == *My* ]]
